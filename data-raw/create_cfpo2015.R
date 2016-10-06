98705d501331f840fccb61ad70b01f8a916c51a9
Eucrow <moko@sierpe.org>
2016-10-06 12:33:07 +0200

    Add cfpo data

---
+++ data-raw/create_cfpo2015.R
@@ -0,0 +1,10 @@
+library(devtools)
+original_wd <- getwd()
+setwd("data-raw")
+cfpo2015 <- read.csv("cfpo2015.csv")
+save(cfpo2015, file = "cfpo2015.RData")
+setwd(original_wd)
+rm(original_wd, tipo_mue)
+devtools::use_data_raw()
+# IMPORTANT: COPY FILE TO /data
+# AND THEN: devtools::use_data()

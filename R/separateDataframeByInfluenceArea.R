a7d0c59b246ff9faeb6b073a780bc058378132cc
Eucrow <moko@sierpe.org>
2016-11-10 10:23:57 +0100

    Create function separateDataframeByInflueceArea.
    This function require the use of areas_influence.RData, created in
    this commit too.

---
+++ R/separateDataframeByInfluenceArea.R
@@ -0,0 +1,32 @@
+#' Separate one dataframe by influence area
+#'
+#' This function separate any dataframe by influence area according to a sireno
+#' port variable available in the dataframe.
+#'
+#'
+#'
+#' @param df is the dataframe to split. It's imperative that contain a variable
+#' with a sireno port code.
+#' @return a list of dataframes for each influence area in the dataframe to
+#' separate.
+#' @export
+#'
+#'
+separateDataframeByInfluenceArea <- function (df, cod_puerto_column){
+
+  if (is.data.frame(df)){
+    data(areas_influencia)
+
+    areas_influencia <- areas_influencia[,c("COD_PUERTO", "AREA")]
+
+    by_area <- merge(df, areas_influencia, by.x = cod_puerto_column, by.y = "COD_PUERTO", all.x = TRUE )
+    by_area <- dlply (by_area, "AREA")
+
+    return(by_area)
+
+  } else {
+
+    stop ("This doesn't like a dataframe")
+  }
+
+}

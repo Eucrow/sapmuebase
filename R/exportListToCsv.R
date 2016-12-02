9a6290c5792a2c85805a248f5f7ca8b5b6af2bcd
Eucrow <moko@sierpe.org>
2016-12-02 11:31:03 +0100

    Fix bug in exportListToCsv and areas_influencia data

--- R/exportListToCsv.R
+++ R/exportListToCsv.R
@@ -1,24 +1,41 @@
 #' Export to csv a list of dataframes
 #'
-#' This function create a csv file for each dataframe in a list of dataframes.
+#' This function create a csv file for each dataframe in a list of dataframes. The
+#' name of every created file contains the name of its corresponding dataframe.
 #'
-#' @param list of dataframes
+#' @param list: list of dataframes
+#' @param prefix: text to add after the name of the dataframe in the file exported. None by default.
+#' @param suffix: text to add before the name of the dataframe in the file exported. None by default.
+#' @param separation: characters to separate the prefix and suffix to the name of
+#' the dataframe. By default is "_".
 #' @return Save in the working directory a csv file for each dataframe in the
 #' list of dataframes.
 #' @export
 #'
 #'
 #'
-exportListToCsv <- function(list){
+exportListToCsv <- function(list, prefix = "", suffix = "", separation = ""){
 
+  # sep_along(list): generate regular sequences. With a list, generates
+  # the sequence 1, 2, ..., length(from). Return a integer vector.
   lapply(seq_along(list), function(i){
 
     if(is.data.frame(list[[i]])){
+
       list_name <- names(list)[[i]]
-      filename <- paste(list_name, ".csv", sep = "")
+
+      if (prefix != "") prefix <- paste0(prefix, separation)
+
+      if (suffix != "") suffix <- paste0(separation, suffix)
+
+      filename <- paste0(prefix, list_name, suffix, ".csv")
+
       write.csv(list[[i]], file = filename, quote = FALSE, row.names = FALSE)
+
     } else {
+
       return(paste("This isn't a dataframe"))
+
     }
 
   })

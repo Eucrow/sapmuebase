a4bd728a143369583e9b8829eb0e268963cf9c16
Eucrow <moko@sierpe.org>
2017-01-19 14:21:02 +0100

    Add documentation to datasets

---
+++ R/estratorim_arte.R
@@ -0,0 +1,13 @@
+#' ESTRATO_RIM - Gear
+#'
+#' A dataset containing the gear of every ESTRATO_RIM used in SAPMUE
+#'
+#' @format A data frame with 3 columns:
+#' \describe{
+#'   \item{ESTRATO_RIM}{}
+#'   \item{ARTE}{Gear of the ESTRATO_RIM}
+#'   \item{COD_ARTE}{Gear code}
+#'   ...
+#' }
+
+"estratorim_arte"
---
+++ R/maestro_flota_sireno.R
@@ -0,0 +1,19 @@
+#' Fleet in SIRENO
+#'
+#' A dataset containing the fleet available in SIRENO
+#'
+#' @format A data frame with 35,942 rows and 10 columns:
+#' \describe{
+#'   \item{BARCOD}{SIRENO code}
+#'   \item{BARCODSECR}{Secretaría General de Pesca code}
+#'   \item{BARMAT}{Registration Number}
+#'   \item{BARDES}{Vessel name}
+#'   \item{BARFOL}{Registration Number}
+#'   \item{BARFECALTA}{Start date}
+#'   \item{BARFECBAJA}{End date}
+#'   \item{BARESTADO}{Code State}
+#'   \item{PAICOD}{Country code}
+#'   \item{PUECOD}{Port base}
+#'   ...
+#' }
+"maestro_flota_sireno"

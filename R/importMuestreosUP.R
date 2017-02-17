00cca3e20e003e6efff0bed056f41f8da000e80d
Eucrow <moko@sierpe.org>
2017-02-17 13:33:00 +0100

    Add function to check months in importMuestreosUP()

--- R/importMuestreosUP.R
+++ R/importMuestreosUP.R
@@ -13,7 +13,6 @@ change_date_format <- function (dataframe){
   dataframe[["FECHA"]] <- format(dataframe[["FECHA"]], "%d-%m-%y")
 }
 
-
 # ---- function to filter by month all the dataframes of muestreos_up list -----
 filter_by_month <- function (dataframe, month){
   # format the month:
@@ -25,6 +24,33 @@ filter_by_month <- function (dataframe, month){
   #return dataframe:
   return(dataframe)
 }
+
+# ---- function to check variable by_month -------------------------------------
+# This function check that the by_by_month variable is a numeric between 1 to 12
+# or FALSE
+check_by_month_argument <- function(by_month) {
+  tryCatch(
+    {
+      if (is.numeric(by_month)){
+        if(by_month < 1 | by_month > 12) {
+          #return ("error")
+          error_text <- paste0("by_month = ", by_month, "??? How many by_months has your culture?")
+          stop(error_text)
+        }
+      } else if ( is.logical(by_month) & by_month == FALSE) {
+        return(by_month)
+      } else {
+        #return ("error")
+        stop(paste0("by_month has to be a numeric between 1 to 12 or FALSE to select all the year"))
+      }
+    },
+    error = function(err) {
+      error_text <- paste0("C'mon boy... ", err)
+      message(error_text)
+    }
+  )
+}
+
 
 # ---- function to import the 'tallas por up' files ----------------------------
 #' Import 'muestreos tallas por up'
@@ -97,7 +123,8 @@ importMuestreosUP <- function(des_tot, des_tal, tal, by_month = FALSE, export = 
   Sys.setlocale("LC_TIME", lct)
 
   # filter by month, only in case by_month == TRUE
-  # TODO: validate by_month between 1 and 12
+  by_month <- check_by_month_argument(by_month)
+
   if (by_month != FALSE){
     muestreos_up <- lapply(muestreos_up, function(x){x <- filter_by_month(x, by_month); x})
   }

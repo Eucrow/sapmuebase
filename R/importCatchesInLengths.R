26f185fb43af0a150200dbc9209b065b056bde1e
Eucrow <moko@sierpe.org>
2017-06-30 09:05:46 +0200

    Factorize importMuestreosUP() with importCatches(), importCatchesInLengths() and importLengths()

---
+++ R/importCatchesInLengths.R
@@ -0,0 +1,37 @@
+# ---- function to import the 'Catches in Lengths' file from 'tallas por up' report ---------
+#' Import the 'Catches in Lengths' file from 'tallas por up' report
+#'
+#' This function import the 'Catches in Lengths' files obtained from 'muestreos tallas por up'
+#' reports in SIRENO.
+#'
+#' Multiple files can be imported at the same time.
+#'
+#' To allow a better use of this data in R, fields 'DIA', 'MES', 'YEAR' and 'TRIMESTRE'
+#' has been created.
+#'
+#' @param file vector with the total landings filenames
+#' @param path path of the files. The working directory by default.
+#' @return Return dataframe.
+#' @export
+importCatchesInLengths <- function(file, path = getwd()){
+  catches_in_lengths <- lapply(
+    file,
+    function(x){
+      tryCatch({
+        fullpath <- paste(path, x, sep="/")
+        type <- formato_variables[formato_variables[["CATCHES_IN_LENGTHS"]]==T, "class_variable_final"]
+        read.table(fullpath, sep=";", header = TRUE, quote = "",
+                   colClasses = type)
+
+      },
+      error = function(err) {
+        error_text <- paste("error in file", x, ": ", err)
+        stop(error_text)
+      }
+      )
+    }
+  )
+  catches_in_lengths <- Reduce(rbind, catches_in_lengths)
+
+  catches_in_lengths <- formatImportedFile(catches_in_lengths)
+}
--- R/importMuestreosUP.R
+++ R/importMuestreosUP.R
@@ -2,7 +2,6 @@
 # return the dataframe corrected
 remove_coma_in_category <- function(dataframe){
   dataframe[["CATEGORIA"]]<- gsub(",", "", dataframe[["CATEGORIA"]])
-  #assign('dataframe',dataframe,.GlobalEnv)#this doesn't work, and I dont know why
   return(dataframe)
 }
 
@@ -18,17 +17,16 @@ add_dates_variables <- function (dataframe){
   dataframe[["FECHA"]] <- as.POSIXlt(dataframe$FECHA, format="%d-%m-%y")
   dataframe[["DIA"]] <- dataframe[["FECHA"]]$mday
   dataframe[["MES"]] <- dataframe[["FECHA"]]$mon+1
-  dataframe[["AÑO"]] <- dataframe[["FECHA"]]$year+1900
+  dataframe[["YEAR"]] <- dataframe[["FECHA"]]$year+1900
   dataframe[["TRIMESTRE"]] <- quarters(dataframe[["FECHA"]])
+
   #remove 'Q' in quarter:
   dataframe[["TRIMESTRE"]] <- substring(dataframe[["TRIMESTRE"]], 2)
   dataframe[["FECHA"]] <- format(dataframe[["FECHA"]], "%d-%m-%y")
 
-
+  #order columns
   dataframe <- dataframe %>%
-    # document() doesn't work using AÑO in this way:
-    # select(COD_ID, FECHA, DIA, MES, AÑO, TRIMESTRE)), everything())
-    select(one_of(c("COD_ID", "FECHA", "DIA", "MES", "AÑO", "TRIMESTRE")), everything())
+    select(one_of(c("COD_ID", "FECHA", "DIA", "MES", "YEAR", "TRIMESTRE")), everything())
 
   return(dataframe)
 }
@@ -46,7 +44,7 @@ filter_by_month <- function (dataframe, month){
 }
 
 # ---- function to check variable by_month -------------------------------------
-# This function check that the by_by_month variable is a numeric between 1 to 12
+# This function check that the by_month variable is a numeric between 1 to 12
 # or FALSE
 check_by_month_argument <- function(by_month) {
   tryCatch(
@@ -73,18 +71,45 @@ check_by_month_argument <- function(by_month) {
   )
 }
 
+# ---- function to format the imported dataframe from muestreos up -------------
+# Is called in the import functions of the tallas_x_up files.
+# Change the format of the FECHA variable and remove the coma in the category
+formatImportedFile <- function(df){
+  # change the column "FECHA" to a date format
+  # to avoid some problems with Spanish_Spain.1252 (or if you are using another
+  # locale), change locale to Spanish_United States.1252:
+  lct <- Sys.getlocale("LC_TIME")
+  Sys.setlocale("LC_TIME","Spanish_United States.1252")
+
+  df[["FECHA"]] <- change_date_format(df)
+
+  df <- add_dates_variables(df)
+
+  # and now the come back to the initial configuration of locale:
+  Sys.setlocale("LC_TIME", lct)
+
+
+  # remove coma in the name of the categories
+  df <- remove_coma_in_category(df)
+
+  return(df)
+}
 
 # ---- function to import the 'tallas por up' files ----------------------------
 #' Import 'muestreos tallas por up'
 #'
-#' This function import the three files obtained from 'muestreos tallas por up' reports in SIRENO.
+#' This function import the three files obtained from 'muestreos tallas por up'
+#' reports in SIRENO. The files are:
+#' - des_tot: information with total landings
+#' - des_tal: landings of the length samples
+#' - tal: lengths
 #'
-#' To allow a better use of this data in R, fields 'DIA', 'MES', 'AÑO' and 'TRIMESTRE'
+#' To allow a better use of this data in R, fields 'DIA', 'MES', 'YEAR' and 'TRIMESTRE'
 #' has been created.
 #'
-#' @param des_tot file with the total landings
-#' @param des_tal file with the landings of the lengths samples
-#' @param tal file with the lengths samples
+#' @param des_tot vector with the total landings filenames
+#' @param des_tal vector with the landings of the lengths samples filenames
+#' @param tal vector with the lengths samples filenames
 #' @param by_month to filter only by one month. Numeric between 1 to 12 to select
 #' one month or FALSE for all the year. FALSE by default.
 #' @param path path of the files. The working directory by default.
@@ -93,80 +118,31 @@ check_by_month_argument <- function(by_month) {
 #' @export
 
 importMuestreosUP <- function(des_tot, des_tal, tal, by_month = FALSE, export = FALSE, path = getwd()){
+
+  # check des_tot, des_tal and tal has the same length
+  if(length(des_tot) != length(des_tal) | length(des_tot) != length(tal)){
+    stop(paste0("the variables", des_tot, ", ", des_tal, ", ", tal, "does not have the same length."))
+  }
+
   # full paths for every file
   fullpath_des_tot <- paste(path, des_tot, sep="/")
   fullpath_des_tal <- paste(path, des_tal, sep="/")
   fullpath_tal <- paste(path, tal, sep="/")
 
-  # import files to data.frame
-  catches <- tryCatch(
-                  read.table(fullpath_des_tot, sep=";", header = TRUE, quote = "",
-                             colClasses = c(NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
-                                            NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
-                                            NA,"factor",NA,NA,NA,NA,NA,"character",NA,NA,
-                                            NA,NA,NA,NA,NA,NA,NA,NA)),
-                  error = function(err) {
-                    error_text <- paste("error in file", des_tot, ": ", err)
-                    stop(error_text)
-                  }
-             )
+  # import files
+  catches <- importCatches(des_tot, PATH_FILES)
 
-  catches_in_lengths <- tryCatch(
-                  read.table(fullpath_des_tal, sep=";", header = TRUE, quote = "",
-                             colClasses = c(NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
-                                            NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
-                                            NA,"factor",NA,NA,NA,NA,NA,"character",NA,NA,
-                                            NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
-                                            NA,NA,NA,NA,NA,NA,NA,NA,NA)),
-                  error = function(err) {
-                    error_text <- paste("error in file", des_tal, ": ", err)
-                    stop(error_text)
-                  }
-             )
+  catches_in_lengths <- importCatchesInLengths(des_tal, PATH_FILES)
 
-  lengths <- tryCatch(
-                  read.table(fullpath_tal, sep=";", header = TRUE, quote = "",
-                             colClasses = c(NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
-                                            NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
-                                            NA,"factor",NA,NA,NA,NA,NA,"character",NA,NA,
-                                            NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
-                                            NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
-                                            NA)),
-                  error = function(err) {
-                    error_text <- paste("error in file", tal, ": ", err)
-                    stop(error_text)
-                  }
-             )
+  lengths <- importLengths(tal, PATH_FILES)
 
-
-  # group in list
+  # group in a list
   muestreos_up<-list(catches_in_lengths=catches_in_lengths, lengths=lengths, catches=catches)
-
-  # change the column "FECHA" to a date format
-  # to avoid some problems with Spanish_Spain.1252 (or if you are using another locale), change locale to Spanish_United States.1252:
-  lct <- Sys.getlocale("LC_TIME")
-  Sys.setlocale("LC_TIME","Spanish_United States.1252")
-
-  # change the format with lapply
-  # it's necesary the last x inside the function, to return the value of x modified
-  muestreos_up <- lapply(muestreos_up, function(x){x[["FECHA"]] <- change_date_format(x); x})
-  muestreos_up <- lapply(muestreos_up, function(x){x[["COD_ARTE"]] <- as.factor(x[["COD_ARTE"]]); x})
-  muestreos_up <- lapply(muestreos_up, function(x){x[["COD_TIPO_MUE"]] <- as.factor(x[["COD_TIPO_MUE"]]); x})
-  muestreos_up <- lapply(muestreos_up, function(x){x[["TALL.PESO"]] <- as.character(x[["TALL.PESO"]]); x})
-
-
-  muestreos_up <- lapply(muestreos_up, function(x){
-                                          x[["COD_ORIGEN"]] <- sprintf("%03d", x[["COD_ORIGEN"]])
-                                          x[["COD_ORIGEN"]] <- as.factor(x[["COD_ORIGEN"]]);
-                                          x})
 
   muestreos_up <- lapply(muestreos_up, function(x){
     x <- add_dates_variables(x)
     x
   })
-
-  # and now the come back to the initial configuration of locale:
-  Sys.setlocale("LC_TIME", lct)
 
   # filter by month, only in case by_month == TRUE
   by_month <- check_by_month_argument(by_month)
@@ -175,21 +151,6 @@ importMuestreosUP <- function(des_tot, des_tal, tal, by_month = FALSE, export = 
     muestreos_up <- lapply(muestreos_up, function(x){x <- filter_by_month(x, by_month); x})
   }
 
-  # remove coma in the name of the categories
-  # I don't know why this doesn't work:
-  # lapply(muestreos_up, function(x){x <- remove_coma_in_category(x)})
-  # so I've to do this:
-  muestreos_up[["catches"]] <- remove_coma_in_category(muestreos_up[["catches"]])
-  muestreos_up[["catches_in_lengths"]] <- remove_coma_in_category(muestreos_up[["catches_in_lengths"]])
-  muestreos_up[["lengths"]] <- remove_coma_in_category(muestreos_up[["lengths"]])
-
-  #convert COD_PUERTO to a character with 4 digits
-  muestreos_up <- lapply(muestreos_up, function(x){
-    x[["COD_PUERTO"]] <- sprintf("%04d", x[["COD_PUERTO"]])
-    x
-  })
-
   #return list
   return(muestreos_up)
 }
-

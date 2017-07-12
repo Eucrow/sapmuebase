b1779ae4a0765ae4a349482a0c8e4af2078c7ac1
Eucrow <moko@sierpe.org>
2017-07-12 13:24:31 +0200

    Fix formato_variable dataset documentation

--- R/formato_variables.R
+++ R/formato_variables.R
@@ -1,8 +1,9 @@
 #' Variables format
 #'
-#' A dataset containing the format of the variables in the tallas_x_up files
+#' A dataset containing the format of the variables in the tallas_x_up report files from SIRENO
+#' with another information structure.
 #'
-#' @format A data frame with 51 rows and 9 columns:
+#' @format A data frame with 9 columns:
 #' \describe{
 #'   \item{name_variable}{name of the variable}
 #'   \item{regex_variable_import}{regular expression of the variable in the import process}
@@ -10,9 +11,9 @@
 #'   \item{mandatory}{TRUE if the variable is mandatory for the import process}
 #'   \item{regex_variable_final}{regular expression that should be the variable after import process}
 #'   \item{class_variable_final}{class that should be the variable after import process}
-#'   \item{CATCHES}{TRUE if the variable is in catches dataframe}
-#'   \item{CATCHES_IN_LENGTHS}{TRUE if the variable is in catches_in_lengths dataframe}
-#'   \item{LENGTHS}{TRUE if the variable is in lengths dataframe}
+#'   \item{CATCHES}{TRUE if the variable is in catches file}
+#'   \item{CATCHES_IN_LENGTHS}{TRUE if the variable is in catches_in_lengths file}
+#'   \item{LENGTHS}{TRUE if the variable is in lengths file}
 #'   ...
 #' }
-"puerto"
+"formato_variables"

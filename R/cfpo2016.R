8e60039c7abff44ff54cc61b4742c70c6f28df25
Eucrow <moko@sierpe.org>
2018-02-08 13:59:39 +0100

    Add cfpo2017 dataset

---
+++ R/cfpo2016.R
@@ -0,0 +1,7 @@
+#' CFPO2016
+#'
+#' A dataset containing the CFPO2016.
+#'
+#' @format A data frame with 34 variables
+
+"cfpo2016"
---
+++ R/cfpo2017.R
@@ -0,0 +1,7 @@
+#' CFPO2017
+#'
+#' A dataset containing the CFPO2016.
+#'
+#' @format A data frame with 28 variables.
+
+"cfpo2017"

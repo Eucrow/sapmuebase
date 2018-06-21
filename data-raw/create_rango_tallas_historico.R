4bfb3ab645f048a30529a87bd637e231b1510dbb
Eucrow <moko@sierpe.org>
2018-06-21 12:15:37 +0200

    Fix rango_tallas_historico dataset

--- data-raw/create_rango_tallas_historico.R
+++ data-raw/create_rango_tallas_historico.R
@@ -1,24 +1,28 @@
 library(devtools)
 library(dplyr)
 original_wd <- getwd()
-setwd("data-raw/data_source_private")
+setwd("data-raw")
 
-file_data <- "IEOUPMUETALSIRENO_2014_2017.TXT"
+file_data <- "data_source_private/IEOUPMUETALSIRENO_2014_2017.TXT"
+
+load("relacion_variables.RData")
+load("formato_variables.RData")
 
 data <- sapmuebase::importRIMLengths(file_data, getwd())
 
-rg <- data %>%
+rango_tallas_historico <- data %>%
+  filter(PROCEDENCIA == "IEO") %>%
   select(COD_ESP_CAT, SEXO, TALLA) %>%
   filter(!is.na(TALLA)) %>% # in some cases, there are lengths with NA
   group_by(COD_ESP_CAT, SEXO) %>%
   summarise(min_length = min(TALLA), max_length = max(TALLA))
 
 
-colnames(rg) <- c("COD_ESP", "SEXO", "TALLA_MIN", "TALLA_MAX")
+colnames(rango_tallas_historico) <- c("COD_ESP", "SEXO", "TALLA_MIN", "TALLA_MAX")
 
-save(rg, file = "rango_tallas_historico.RData")
-setwd(original_wd)
-rm(original_wd, rg)
+devtools::use_data(rango_tallas_historico, overwrite = TRUE)
 devtools::use_data_raw()
-# IMPORTANT: COPY FILE TO /data
-# AND THEN: devtools::use_data()
+
+# save(rg, file = "rango_tallas_historico.RData")
+# setwd(original_wd)
+# rm(original_wd, rg)

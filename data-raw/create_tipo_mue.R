a78754a5011c936ccde3a45d6a6afbd699fb539e
Eucrow <moko@sierpe.org>
2019-01-31 14:21:17 +0100

    Remove useless files and update tipo_mue dataset with type 3

--- data-raw/create_tipo_mue.R
+++ data-raw/create_tipo_mue.R
@@ -1,10 +1,20 @@
 library(devtools)
+
+usethis::use_data_raw()
+
 original_wd <- getwd()
 setwd("data-raw")
+
 tipo_mue <- read.csv("tipo_mue.csv", colClasses = c("factor", "factor", "factor"))
-save(tipo_mue, file = "tipo_mue.RData")
+
+# use_data() create the file in /data (before, we have to copy manually, but not
+# rigth now). The file created has extension .rda, instead of .Rdata.
+usethis::use_data(tipo_mue, overwrite = TRUE)
+
+# we have to make sure there aren't a .Rdata file with the same name than the
+# .rda file just created, because R read firts the .Rdata and ignore the .rda
+
+devtools::document()
+
 setwd(original_wd)
 rm(original_wd, tipo_mue)
-devtools::use_data_raw()
-# IMPORTANT: COPY FILE TO /data
-# AND THEN: devtools::use_data()

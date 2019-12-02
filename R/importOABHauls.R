dacd5425f1c9779a8948c0b754879e9ffd31a8ad
Eucrow <moko@sierpe.org>
2019-12-02 13:08:22 +0100

    Change format of date fields returned in importOABHauls function

--- R/importOABHauls.R
+++ R/importOABHauls.R
@@ -52,4 +52,19 @@ importOABHauls <- function(file, path = getwd()){
 
   hauls <- formatVariableTypes(hauls, file_type)
 
+  # Change FECHA_LAR y FECHA_VIR format from 16-JUN-19 TO 16/06/2019
+  # to avoid some problems with Spanish_Spain.1252 (or if you are using another
+  # locale), change locale to Spanish_United States.1252:
+  lct <- Sys.getlocale("LC_TIME")
+  Sys.setlocale("LC_TIME","Spanish_United States.1252")
+
+  hauls$FECHA_LAR <- format(as.Date(hauls$FECHA_LAR, "%d-%b-%y"), "%d/%m/%Y")
+  hauls$FECHA_VIR <- format(as.Date(hauls$FECHA_VIR, "%d-%b-%y"), "%d/%m/%Y")
+
+  # and now the come back to the initial configuration of locale:
+  Sys.setlocale("LC_TIME", lct)
+
+  return(hauls)
+
+
 }

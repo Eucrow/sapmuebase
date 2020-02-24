ca926583210852ad55a31eed772311f12542da9e
Eucrow <moko@sierpe.org>
2020-02-24 14:30:47 +0100

    Create rango_tallas_historico_OAB and rango_tallas_historico_RIM datasets. Remove old rango_tallas_historico dataset

---
+++ R/rango_tallas_historico_RIM.R
@@ -0,0 +1,16 @@
+#' Historical size range by species in RIM samples.
+#'
+#' A dataset containing the historical size range by species and sex in RIM
+#' samples. The dataset is created in create_rango_tallas_historico.R, using
+#' lengths recorded in SIRENO between 2014 and 2019 from RIM samples.
+#'
+#' @format A data frame with 4 columns:
+#' \describe{
+#'   \item{COD_ESP}{SIRENO code species}
+#'   \item{SEXO}{sex}
+#'   \item{TALLA_MIN}{minimun length}
+#'   \item{TALLA_MAX}{maximun length}
+#'   ...
+#' }
+
+"rango_tallas_historico_RIM"

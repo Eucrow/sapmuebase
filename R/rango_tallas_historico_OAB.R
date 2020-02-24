264c3da817f0a21ea6215c7a1268a98ec75944d3
Eucrow <moko@sierpe.org>
2020-02-24 14:30:47 +0100

    Create rango_tallas_historico_OAB and rango_tallas_historico_RIM datasets. Remove old rango_tallas_historico dataset

---
+++ R/rango_tallas_historico_OAB.R
@@ -0,0 +1,16 @@
+#' Historical size range by species in OAB samples.
+#'
+#' A dataset containing the historical size range by species and sex in OAB
+#' samples. The dataset is created in create_rango_tallas_historico.R, using
+#' lengths recorded in SIRENO between 2014 and 2018 from OAB samples.
+#'
+#' @format A data frame with 4 columns:
+#' \describe{
+#'   \item{COD_ESP}{SIRENO code species}
+#'   \item{SEXO}{sex}
+#'   \item{TALLA_MIN}{minimun length}
+#'   \item{TALLA_MAX}{maximun length}
+#'   ...
+#' }
+
+"rango_tallas_historico_OAB"
--- data-raw/create_rango_tallas_historico_RIM.R
+++ data-raw/create_rango_tallas_historico_RIM.R
@@ -17,7 +17,7 @@ lengths_data_2018 <- sapmuebase::importRIMLengths(file_data_2018, getwd())
 
 lengths_data <- rbind(lengths_data_2014_2017, lengths_data_2018)
 
-rango_tallas_historico <- lengths_data %>%
+rango_tallas_historico_RIM <- lengths_data %>%
   filter(PROCEDENCIA == "IEO") %>%
   select(COD_ESP_CAT, SEXO, TALLA) %>%
   group_by(COD_ESP_CAT, SEXO) %>%
@@ -25,13 +25,13 @@ rango_tallas_historico <- lengths_data %>%
   mutate(min_length = ifelse(is.infinite(min_length), NA, min_length)) %>%
   mutate(max_length = ifelse(is.infinite(max_length), NA, max_length))
 
-colnames(rango_tallas_historico) <- c("COD_ESP", "SEXO", "TALLA_MIN", "TALLA_MAX")
+colnames(rango_tallas_historico_RIM) <- c("COD_ESP", "SEXO", "TALLA_MIN", "TALLA_MAX")
 
 # use_data() create the file in /data
-usethis::use_data(rango_tallas_historico, overwrite = TRUE)
+usethis::use_data(rango_tallas_historico_RIM, overwrite = TRUE)
 
 devtools::document()
 
 setwd(original_wd)
-rm(original_wd, rango_tallas_historico, file_data_2014_2017, file_data_2018,
+rm(original_wd, rango_tallas_historico_RIM, file_data_2014_2017, file_data_2018,
    lengths_data_2014_2017, lengths_data_2018, lengths_data)

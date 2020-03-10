e572cc0fe6f138bdea0d6e07eefd579e7d0c0a65
Eucrow <moko@sierpe.org>
2020-03-10 12:16:40 +0100

    Create p99_capturas_historico dataset

--- R/p97_percentil_historico.R
+++ R/p97_percentil_historico.R
@@ -1,15 +1,14 @@
 #' Historical percentile 97 range by species.
 #'
-#' A dataset containing the historical percentile 90 range by species. The dataset is created
+#' A dataset containing the historical percentile 97 of species catcges. The dataset is created
 #' in create_p97_capturas_historico.R, using catches recorded in SIRENO between 2014
-#' to 207.
+#' to 2018.
 #'
 #' @format A data frame with 4 columns:
 #' \describe{
 #'   \item{COD_ESP}{SIRENO code species}
-#'   \item{SEXO}{sex}
-#'   \item{TALLA_MIN}{minimun length}
-#'   \item{TALLA_MAX}{maximun length}
+#'   \item{ESTRATO_RIM}{RIM stratum}
+#'   \item{P97}{percentile 97}
 #'   ...
 #' }
 

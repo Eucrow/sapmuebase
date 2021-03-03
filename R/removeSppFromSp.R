d419177bf8552624a4e5500887f3e066d9ab64aa
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2021-03-03 14:38:59 +0100

    Update documentation and version number.

--- R/removeSppFromSp.R
+++ R/removeSppFromSp.R
@@ -1,6 +1,6 @@
-#' Remove " spp" abbreviation in species.
-#' @param sp a character vector of species.
-#' @return a character vector of species without " spp".
+#' @title Remove " spp" abbreviation in species.
+#' @param sp A character vector of species.
+#' @return A character vector of species without " spp".
 #' @author Marco A. Ámez , \email{marco.amez@@ieo.es}
 removeSppFrom_Sp <- function(sp){
   return (gsub(" spp$", "", sp))

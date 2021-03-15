ff0b691fe856d76a6d9c35b09ac21a3901045abb
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2021-03-15 14:55:16 +0100

    Update categorias dataset.

---
+++ R/categorias.R
@@ -0,0 +1,16 @@
+#' Categories in SIRENO
+#'
+#' A dataset containing the categories available in SIRENO. The original data
+#' is obtained from SIRENO (report by proyect --> categories)
+#'
+#' @format A data frame with 28958 rows and 7 columns:
+#' \describe{
+#'   \item{COD_PUERTO}{Port code}
+#'   \item{PUERTO}{ Port name}
+#'   \item{COD_ESP}{SIRENO species code}
+#'   \item{ESP}{Species name}
+#'   \item{COD_CATEGORIA}{Category code}
+#'   \item{CATEGORIA}{Category name}
+#'   \item{PROCESO}{Presentation (whole, gutted...)}
+#'   ...
+#' }

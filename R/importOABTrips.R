6aa39076b51d608a72d2e247bf437ea56ba14ccb
Marco A. Ámez <marco.a.amez@gmail.com>
2023-03-17 12:46:50 +0100

    Fix initial and final date and time trips variables.

--- R/importOABTrips.R
+++ R/importOABTrips.R
@@ -40,6 +40,7 @@ importOABTrips <- function(file, path = getwd()){
 
   trips <- sapmuebase:::renameFileVariables(trips, file_type)
 
+
   # This file has variables with comma as a decimal character: TRB, ESLORA
   trips <- sapmuebase:::replace_coma_with_dot(trips, "TRB")
   trips <- sapmuebase:::replace_coma_with_dot(trips, "ESLORA")
@@ -52,16 +53,17 @@ importOABTrips <- function(file, path = getwd()){
   lct <- Sys.getlocale("LC_TIME")
   Sys.setlocale("LC_TIME","Spanish_United States.1252")
 
-  # create fields with data and time format
-
+  # format properly date variables
   trips$FECHA_EMB <- format(as.Date(trips$FECHA_EMB, "%d-%b-%y"), "%d/%m/%Y")
   trips$FECHA_DESEM <- format(as.Date(trips$FECHA_DESEM, "%d-%b-%y"), "%d/%m/%Y")
 
-  trips[["FECHA_HORA_EMB"]] <- paste(trips[["FECHA_EMB"]], trips[["HORA_INI"]])
-  trips[["FECHA_HORA_EMB"]] <- as.POSIXct(trips[["FECHA_HORA_EMB"]], format="%d/%m/%Y %H:%M")
+  # create fields with data and time format
+  trips[["FECHA_HORA_INI_MAREA"]] <- paste(trips[["FECHA_INI_MAREA"]], trips[["HORA_INI_MAREA"]])
+  trips[["FECHA_HORA_INI_MAREA"]] <- as.POSIXct(trips[["FECHA_HORA_INI_MAREA"]], format="%d/%m/%Y %H:%M")
 
-  trips[["FECHA_HORA_DESEM"]] <- paste(trips[["FECHA_DESEM"]], trips[["HORA_FIN"]])
-  trips[["FECHA_HORA_DESEM"]] <- as.POSIXct(trips[["FECHA_HORA_DESEM"]], format="%d/%m/%Y %H:%M")
+  trips[["FECHA_HORA_FIN_MAREA"]] <- paste(trips[["FECHA_FIN_MAREA"]], trips[["HORA_FIN_MAREA"]])
+  trips[["FECHA_HORA_FIN_MAREA"]] <- as.POSIXct(trips[["FECHA_HORA_FIN_MAREA"]], format="%d/%m/%Y %H:%M")
+
 
   # and come back to the initial configuration of locale:
   Sys.setlocale("LC_TIME", lct)

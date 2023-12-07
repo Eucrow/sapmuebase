3d46367a4ddc63296e5aa6600cc69c2e979848e4
Marco A. Ámez <marco.a.amez@gmail.com>
2023-12-07 13:45:16 +0100

    Create metier_coherence data set.

---
+++ R/metier_coherence.R
@@ -0,0 +1,17 @@
+#' Relation between METIER_DCF, CALADERO_DCF, ORIGEN and ARTE
+#'
+#' A data set containing the relation between METIER_DCF, CALADERO_DCF, ORIGEN
+#' and ARTE.
+#'
+#' @format A data frame with 7 columns:
+#' \describe{
+#'   \item{METIER_DCF}{}
+#'   \item{CALADERO_DCF}{}
+#'   \item{ESTRATO_RIM}{}
+#'   \item{COD_ORIGEN}{}
+#'   \item{ORIGEN}{}
+#'   \item{COD_ARTE}{}
+#'   \item{ARTE}{}
+#'   ...
+#' }
+"metier_coherence"

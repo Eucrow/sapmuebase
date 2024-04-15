b6c00f305dfc36d0376298b0227819a5650def47
Marco A. Ámez <marco.a.amez@gmail.com>
2024-04-15 11:17:51 +0200

    Update categories data set.

--- R/categorias.R
+++ R/categorias.R
@@ -3,14 +3,15 @@
 #' A dataset containing the categories available in SIRENO. The original data
 #' is obtained from SIRENO (report by project --> categories)
 #'
-#' @format A data frame with 28958 rows and 7 columns:
+#' @format A data frame with this columns:
 #' \describe{
+#'   \item{COD_CATEGORIA}{Category code}
+#'   \item{CATEGORIA}{Category name}
 #'   \item{COD_PUERTO}{Port code}
 #'   \item{PUERTO}{ Port name}
 #'   \item{COD_ESP}{SIRENO species code}
 #'   \item{ESP}{Species name}
-#'   \item{COD_CATEGORIA}{Category code}
-#'   \item{CATEGORIA}{Category name}
+#'   \item{ALFA3}{FAO code}
 #'   \item{PROCESO}{Presentation (whole, gutted...)}
 #'   ...
 #' }
--- data-raw/create_categorias.R
+++ data-raw/create_categorias.R
@@ -11,7 +11,7 @@ library(devtools)
 
 original_wd <- getwd()
 setwd("data-raw")
-categorias <- read.csv("IEOCATPUE3MARCO_2024_03.TXT",
+categorias <- read.csv("IEOCATPUE3MARCO_2024_04.TXT",
                      sep=";",
                      skip = 6,
                      header = TRUE,
